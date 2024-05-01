7a33a5d87a7ef5a81c2a1f2af9b45942adec0de1
Alexandra Aggelidis <alexandra.aggelidis@engineering.digital.dwp.gov.uk>
2024-05-01 17:35:23 +0100

    wt-4485-wfm1b-4-01
    - MOJ front end installed
    - test-list.html added to explore filter design
    - MI tile removed from choose a task page
    
    GitOrigin-RevId: b205cc1413b94026025760ad67ca153c0bef3d64

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,21 @@ function printCheck() {
 window.GOVUKPrototypeKit.documentReady(function () {
 })
 
+
+new MOJFrontend.FilterToggleButton({
+  bigModeMediaQuery: "(min-width: 48.063em)",
+  startHidden: true,
+  toggleButton: {
+    container: document.querySelector(".moj-action-bar__filter"),
+    showText: "Show filter",
+    hideText: "Hide filter",
+    classes: "govuk-button--secondary",
+  },
+  closeButton: {
+    container: document.querySelector(".moj-filter__header-action"),
+    text: "Close",
+  },
+  filter: {
+    container: document.querySelector(".moj-filter"),
+  },
+});
